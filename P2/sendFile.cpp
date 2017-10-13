93559d027b83d6264af2fac771c75025d76028e3
jx17 <junruxia@hotmail.com>
2017-10-13 16:59:30 -0500

    sendFile c++ version

---
+++ P2/sendFile.cpp
@@ -0,0 +1,83 @@
+//
+//  main.cpp
+//  P2
+//
+//  Created by 夏俊如 on 10/13/17.
+//  Copyright © 2017 Junru Xia. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+
+using namespace std;
+vector<string> split(const string &s, char delim) {
+    stringstream ss(s);
+    string item;
+    vector<string> tokens;
+    while (getline(ss, item, delim)) {
+        tokens.push_back(item);
+    }
+    return tokens;
+}
+
+int main(int argc, const char * argv[]) {
+    // deal with input
+    if (argc != 5) {
+        cout << "Please provide comprehensive information\n";
+        return 0;
+    }
+    string rflag = argv[1];
+    string host_port = argv[2];
+    string fflag = argv[3];
+    string file_path = argv[4];
+    
+    
+    /* our client socket */
+    int sock;
+    
+    /* address structure for identifying the server */
+    struct sockaddr_in sin;
+ 
+    /* receiver host */
+    vector<string> host_port_vec = split(host_port, ':');
+    struct hostent *host = gethostbyname(host_port_vec[0].c_str());
+    unsigned int server_addr = *(unsigned int *) host->h_addr_list[0];
+
+    
+    /* receiver port number */
+    unsigned short server_port = stoi (host_port_vec[1]);
+
+    /* get path and file name */
+    vector<string> file_path_vec = split(file_path, '/');
+    int file_path_size = (int)file_path_vec.size();
+    string fileName = file_path_vec[file_path_size-1];
+    string path = file_path.substr(0, file_path.length() - fileName.length());
+  
+    /* create a socket */
+    if ((sock = socket (PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+    {
+        perror ("opening UDP socket");
+        abort ();
+    }
+    
+    /* fill in the server's address */
+    memset (&sin, 0, sizeof (sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = server_addr;
+    sin.sin_port = htons(server_port);
+    
+    return 0;
+}
