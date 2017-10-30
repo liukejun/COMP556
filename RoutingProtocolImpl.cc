e3492742b6c549c1c1ee877f2b28c7d57b7d1cb3
Shuo Zhao <zhaoshuosve@gmail.com>
2017-10-30 17:30:32 -0500

    Implement packet parsing and preparation utilities

--- RoutingProtocolImpl.cc
+++ RoutingProtocolImpl.cc
@@ -1,4 +1,49 @@
+#include <cstdint>
+
+#include <arpa/inet.h>
+
 #include "RoutingProtocolImpl.h"
+#include "global.h"
+
+//
+// Small utilities.
+//
+
+Packet parse_packet(Port_id port, void* packet)
+{
+    Packet res;
+
+    res.type = *static_cast<ePacketType*>(packet);
+    res.port = port;
+
+    auto packet_16 = static_cast<uint16_t*>(packet);
+    res.size = ntohs(packet_16[1]);
+    res.src = ntohs(packet_16[2]);
+    res.dest = ntohs(packet_16[3]);
+
+    res.payload = static_cast<void*>(packet_16 + 4);
+    res.packet = packet;
+
+    return res;
+}
+
+void* prepare_packet(
+    ePacketType type, Packet_size size, Router_id src, Router_id dest)
+{
+    uint8_t* packet = new uint8_t[size];
+    packet[0] = type;
+    packet[1] = 0;
+
+    auto packet_16 = static_cast<uint16_t*>(packet);
+    packet_16[1] = htons(size);
+    packet_16[2] = htons(src);
+    packet_16[3] = htons(dest);
+
+    return static_cast<void*>(packet);
+}
+
+const size_t PACKET_HEADER_SIZE = 8;
+
 
 RoutingProtocolImpl::RoutingProtocolImpl(Node* n)
     : RoutingProtocol(n)
