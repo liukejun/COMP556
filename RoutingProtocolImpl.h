473449d3b3a6109b40453ec78919faf014b54c19
Shuo Zhao <zhaoshuosve@gmail.com>
2017-10-30 15:19:18 -0500

    Clarify structure and naming
    The naming has been made more consistent and the structure is cleaned up
    for better handling of different cases.

--- RoutingProtocolImpl.h
+++ RoutingProtocolImpl.h
@@ -48,13 +48,13 @@ using Packet_size = unsigned short;
 
 struct Port_stat {
     bool if_conn;
-    Router_id id;
+    Router_id router_id;
     Time rtt;
     Time last_update;
 
     Port_stat()
         : if_conn(false)
-        , id(0)
+        , router_id(0)
         , rtt(0)
         , last_update(0)
     {
@@ -66,12 +66,12 @@ struct Port_stat {
 
 using Port_stats = std::vector<Port_stat>;
 
-/** The set of neighbours of a router.
+/** The set of active ports of a router.
  *
  * The contents are the indices to the port status vector.
  */
 
-using Neighbs = std::unordered_set<size_t>;
+using Active_ports = std::unordered_set<size_t>;
 
 //
 // Core routing data structures.
@@ -80,9 +80,17 @@ using Neighbs = std::unordered_set<size_t>;
 /** Single entry inside an forward table.
  */
 
-struct Forward_entry {
-    Router_id next;
+struct Forward {
+    Router_id router_id;
+    Port_id port_id;
     Time cost;
+
+    Forward()
+        : router_id(-1)
+        , port_id(-1)
+        , cost(INFINITY_COST)
+    {
+    }
 };
 
 /** The main forwarding table.
@@ -91,33 +99,58 @@ struct Forward_entry {
  * DV for the router itself in DV routing.
  */
 
-using Forward_table = std::unordered_map<Router_id, Forward_entry>;
+using Forward_table = std::unordered_map<Router_id, Forward>;
 
 //
 // Events related: messages and alarms.
 //
 
-/** Get the type of a packet.
+/** The content of an incoming packet.
+ *
+ * Note that this struct just serve to hold the parsed content and does *not*
+ * take the ownership of the packet.
  */
 
-ePacketType get_packet_type(void*);
+struct Packet {
+    ePacketType type;
+    Port_id port;
+    Packet_size size;
+    Router_id src;
+    Router_id dest;
+    void* payload;
+    void* packet;
+};
+
+/** Parse the given packet.
+ */
+
+Packet parse_packet(Port_id, void*);
+
+/** Prepare a packet to send.
+ *
+ * New memory is allocated and the ownership is returned.
+ */
+
+void* prepare_packet(
+    ePacketType type, Packet_size size, Router_id src, Router_id dest);
+
+/** Size of the header of a packet.
+ */
+
+const size_t PACKET_HEADER_SIZE;
 
 /** Types for different kinds of alarms.
+ *
+ * CHK types are for checking the activity of a port, DV entry, or an LS entry.
+ * The REQ types are generally for periodic actions to be performed.
  */
 
-enum class Alarm_type {
-    PING_TIMEOUT,
-    PING_REQ,
-    DV_TIMEOUT,
-    DV_REQ,
-    LS_TIMEOUT,
-    LS_REQ
-};
+enum class Alarm_type { PORT_CHK, PING_REQ, DV_CHK, DV_REQ, LS_CHK, LS_REQ };
 
 /** The actual alarms.
  *
- * In the current implementation, alarm will be allocated by the sender and
- * released by the receiver.
+ * In the current implementation, alarm will be allocated by the sender on the
+ * heap and released by the handler.
  */
 
 struct Alarm {
@@ -126,13 +159,13 @@ struct Alarm {
     union {
         /** Port index.
          *
-         * For PING_TIMEOUT.
+         * For PORT_CHK.
          */
         size_t port_id;
 
         /** Router ID.
          *
-         * For DV_TIMEOUT and LS_TIMEOUT.
+         * For DV_CHK and LS_CHK.
          */
         Router_id router_id;
 
@@ -145,19 +178,55 @@ struct Alarm {
 
 class RoutingProtocolImpl;
 
+/** Changes possible on a neighbour.
+ */
+
+enum class Neighb_delta { RTT_INCR, RTT_DECR, DISCONN };
+
 /** The distance-vector router.
  */
 
 class DV_router {
 public:
-    DV_router(RoutingProtocolImpl&);
+    DV_router(RoutingProtocolImpl& rp)
+        : rp_{ rp }
+        , dvs_{}
+    {
+    }
 
-    void update_neighb(Router_id id, Time rtt);
-    void remove_neighb(Router_id id, Time rtt);
+    void update_neighb(
+        Port_id port_id, Router_id router_id, Time rtt, Neighb_delta);
 
-    bool recv_dv(Port_id, void*, Packet_size);
+    /** Handle new dv received.
+     */
+
+    bool recv_dv(Packet&);
+
+    /** Send DV to its neighbours.
+     *
+     * Any traffic through the poisoned port is given an infinite cost.
+     */
+
+    void send_dv(Port_id poisoned);
+
+    /** Check the status of a router.
+     */
+
+    void chk_stat(Router_id router);
 
 private:
+    /** Compute the distance vector.
+     *
+     * The return value indicates if there are changes happened.
+     */
+
+    bool compute_dv();
+
+    static const Time DV_SEND_INTERV;
+    static const Time DV_OUT_TIME;
+
+    RoutingProtocolImpl& rp_;
+
     /** Distance vector from other routers.
      */
 
@@ -178,10 +247,31 @@ private:
 
 class LS_router {
 public:
-    LS_router(RoutingProtocolImpl&);
-    void update_neighb(Router_id id, Time rtt);
-    void remove_neighb(Router_id id, Time rtt);
-    bool recv_ls(Port_id, void*, Packet_size);
+    LS_router(RoutingProtocolImpl& rp)
+        : rp_{ rp }
+    {
+    }
+
+    void update_neighb(
+        Port_id port_id, Router_id router_id, Time rtt, Neighb_delta delta);
+
+    /** Handle an incoming LS packet.
+     */
+
+    bool recv_ls(Packet&);
+
+    /** Check the status of a fellow router.
+     */
+
+    void chk_stat(Router_id);
+
+    /** Broadcast your own link status.
+     */
+
+    void bcast_ls();
+
+private:
+    RoutingProtocolImpl& rp_;
 };
 
 /** Implementation of the routing protocol interface.
@@ -231,6 +321,8 @@ public:
 
 private:
     /** Ping all ports of the router.
+     *
+     * This method sends a ping to all the ports and schedule the next ping.
      */
 
     void ping_ports();
@@ -238,35 +330,54 @@ private:
     /** Check if the given port is still alive.
      */
 
-    void check_port(Port_id port);
+    void chk_port(Port_id port);
+
+    /** Disassociate a port with its peer.
+     *
+     * The given port does not have to be connected to a peer already.  When it
+     * is connected, the connection will be removed.
+     */
+
+    void disassoc_port(Port_id port);
+
+    /** Schedule a check on the given port.
+     *
+     * The check is scheduled after the timeout cut-off has been exceeded.
+     */
+
+    void sched_port_chk(Port_id port);
 
     /** Handle incoming data packet.
      */
 
-    bool recv_data(Port_id, void*, Packet_size);
+    bool recv_data(Packet&);
 
     /** Handle incoming ping packet.
      */
 
-    bool recv_ping(Port_id, void*, Packet_size);
+    bool recv_ping(Packet&);
 
     /** Handle incoming pong packet.
      */
 
-    bool recv_pong(Port_id, void*, Packet_size);
+    bool recv_pong(Packet&);
+
+    static const Time PING_INTERV;
+    static const Time PING_OUT_TIME;
 
     Node* sys; // To store Node object; used to access GSR9999 interfaces
 
     unsigned short router_id_;
     eProtocolType protocol_type_;
 
-    Port_stats port_stats_;
-    Forward_table forward_table_;
+    Port_stats port_stats_{};
+    Active_ports active_ports_{};
+    Forward_table forward_table_{};
 
     friend class DV_router;
     friend class LS_router;
-    std::unique_ptr<DV_router> dv_router_;
-    std::unique_ptr<LS_router> ls_router_;
+    std::unique_ptr<DV_router> dv_router_{};
+    std::unique_ptr<LS_router> ls_router_{};
 };
 
 #endif
